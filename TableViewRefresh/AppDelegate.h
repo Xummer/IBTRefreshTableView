117834998785027e1baa79784b185b6c17491283
xummer <willx@gobeta.com.cn>
2014-03-27 16:29:14 +0800

    Initial Commit

---
+++ TableViewRefresh/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TableViewRefresh
+//
+//  Created by Xummer on 14-3-27.
+//  Copyright (c) 2014年 Xummer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TableViewRefresh/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TableViewRefresh
+//
+//  Created by Xummer on 14-3-27.
+//  Copyright (c) 2014年 Xummer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
